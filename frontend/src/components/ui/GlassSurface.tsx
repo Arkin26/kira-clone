812f4374345e1fd26d6f888b0b1b0028b7846c07
Arkin26 <arkinutmal@gmail.com>
2026-04-20 20:24:26 +0530

    ninth

--- frontend/src/components/ui/GlassSurface.tsx
+++ frontend/src/components/ui/GlassSurface.tsx
@@ -242,6 +242,7 @@ const GlassSurface: React.FC<GlassSurfaceProps> = ({
              0px 8px 24px rgba(17, 17, 26, 0.05) inset,
              0px 16px 56px rgba(17, 17, 26, 0.05) inset`
       };
+    }
     if (isDarkMode) {
       if (!backdropFilterSupported) {
         return {
